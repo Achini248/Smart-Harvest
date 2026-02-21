ff5aa49a9a7e82c1c51f0d7ff4afb57139e5fe56
Achini248 <nimeshikaachini22@gmail.com>
2026-02-21 09:44:38 +0530

    update auth_repository_impl.dart

--- lib/features/authentication/data/repositories/auth_repository_impl.dart
+++ lib/features/authentication/data/repositories/auth_repository_impl.dart
@@ -1,32 +1,8 @@
-// lib/features/authentication/data/repositories/auth_repository_impl.dart
+import '../entities/user.dart';
 
-import '../../domain/entities/user.dart';
-import '../../domain/repositories/auth_repository.dart';
-import '../datasources/auth_remote_datasource.dart';
-
-class AuthRepositoryImpl implements AuthRepository {
-  final AuthRemoteDataSource remoteDataSource;
-  AuthRepositoryImpl({required this.remoteDataSource});
-
-  @override
-  Future<User?> getCurrentUser() => remoteDataSource.getCurrentUser();
-
-  @override
-  Future<User> login(String email, String password) =>
-      remoteDataSource.login(email, password);
-
-  @override
-  Future<User> register(String email, String password, String phone) =>
-      remoteDataSource.register(email, password, phone);
-
-  @override
-  Future<void> logout() => remoteDataSource.logout();
-
-  @override
-  Future<String> sendOtp(String phoneNumber) =>
-      remoteDataSource.sendOtp(phoneNumber);
-
-  @override
-  Future<User> verifyOtp(String verificationId, String otpCode) =>
-      remoteDataSource.verifyOtp(verificationId, otpCode);
+abstract class AuthRepository {
+  Future<User> login(String email, String password);
+  Future<User> register(String email, String password, String phoneNo);
+  Future<void> logout();
+  Future<User?> getCurrentUser();
 }
