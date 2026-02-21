afc3f0876538988bdc07e13dd209a6ea8283982f
Achini248 <nimeshikaachini22@gmail.com>
2026-02-21 11:05:04 +0530

    fixed auth_event.dart

--- lib/features/authentication/presentation/bloc/auth_event.dart
+++ lib/features/authentication/presentation/bloc/auth_event.dart
@@ -1,80 +1,37 @@
-// lib/features/authentication/presentation/bloc/auth_event.dart
-
 import 'package:equatable/equatable.dart';
 
 abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+
   @override
   List<Object?> get props => [];
 }
 
-class AuthCheckStatusEvent extends AuthEvent {}
-
-class AuthLoginEvent extends AuthEvent {
+class LoginEvent extends AuthEvent {
   final String email;
   final String password;
-  AuthLoginEvent(this.email, this.password);
+
+  const LoginEvent({required this.email, required this.password});
 
   @override
   List<Object?> get props => [email, password];
 }
 
-class AuthRegisterEvent extends AuthEvent {
+class RegisterEvent extends AuthEvent {
   final String email;
   final String password;
-  final String phone;
-  AuthRegisterEvent(this.email, this.password, this.phone);
+  final String phoneNo;
 
-  @override
-  List<Object?> get props => [email, password, phone];
-}
-
-class AuthLogoutEvent extends AuthEvent {}
-
-class AuthSendOtpEvent extends AuthEvent {
-  final String phoneNumber;
-  AuthSendOtpEvent(this.phoneNumber);
-
-  @override
-  List<Object?> get props => [phoneNumber];
-}
-
-class AuthVerifyOtpEvent extends AuthEvent {
-  final String verificationId;
-  final String otpCode;
-  AuthVerifyOtpEvent(this.verificationId, this.otpCode);
-
-  @override
-  List<Object?> get props => [verificationId, otpCode];
-}
-
-class AuthResendOtpEvent extends AuthEvent {
-  final String phoneNumber;
-  AuthResendOtpEvent(this.phoneNumber);
-
-  @override
-  List<Object?> get props => [phoneNumber];
-}
-
-class AuthTimerTickEvent extends AuthEvent {
-  final String verificationId;
-  final String phoneNumber;
-  final int secondsLeft;
-  AuthTimerTickEvent(this.verificationId, this.phoneNumber, this.secondsLeft);
-
-  @override
-  List<Object?> get props => [verificationId, phoneNumber, secondsLeft];
-}
-
-class AuthUpdateProfileEvent extends AuthEvent {
-  final String displayName;
-  final String phone;
-  final String role;
-  AuthUpdateProfileEvent({
-    required this.displayName,
-    required this.phone,
-    required this.role,
+  const RegisterEvent({
+    required this.email,
+    required this.password,
+    required this.phoneNo,
   });
 
   @override
-  List<Object?> get props => [displayName, phone, role];
+  List<Object?> get props => [email, password, phoneNo];
 }
+
+class LogoutEvent extends AuthEvent {}
+
+class CheckAuthStatusEvent extends AuthEvent {}
