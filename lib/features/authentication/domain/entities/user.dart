a48e6db24ddb124b2eaca7f7bdbe93be242dbfd4
Achini248 <nimeshikaachini22@gmail.com>
2026-02-21 09:31:57 +0530

    update user.dart

--- lib/features/authentication/domain/entities/user.dart
+++ lib/features/authentication/domain/entities/user.dart
@@ -1,18 +1,25 @@
-// lib/features/authentication/domain/entities/user.dart
-
+// user.dart
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
   final String id;
   final String email;
-  final String? phone;
+  final String? name;
+  final String? phoneNo;
+  final String? location;
+  final String? profilePhotoUrl;
+  final String role; // 'farmer', 'buyer', 'officer'
 
   const User({
     required this.id,
     required this.email,
-    this.phone,
+    this.name,
+    this.phoneNo,
+    this.location,
+    this.profilePhotoUrl,
+    this.role = 'farmer',
   });
 
   @override
-  List<Object?> get props => [id, email, phone];
+  List<Object?> get props => [id, email, name, phoneNo, location, profilePhotoUrl, role];
 }
