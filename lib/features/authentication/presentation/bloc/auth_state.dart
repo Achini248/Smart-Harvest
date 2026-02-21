5c1a6eeb81be9a6374303fb7d4de8a0e12f55c01
Achini248 <nimeshikaachini22@gmail.com>
2026-02-21 11:05:39 +0530

    fixed auth_state.dart

--- lib/features/authentication/presentation/bloc/auth_state.dart
+++ lib/features/authentication/presentation/bloc/auth_state.dart
@@ -1,9 +1,9 @@
-// lib/features/authentication/presentation/bloc/auth_state.dart
-
 import 'package:equatable/equatable.dart';
 import '../../domain/entities/user.dart';
 
 abstract class AuthState extends Equatable {
+  const AuthState();
+
   @override
   List<Object?> get props => [];
 }
@@ -12,56 +12,21 @@ class AuthInitial extends AuthState {}
 
 class AuthLoading extends AuthState {}
 
-class AuthUnauthenticated extends AuthState {}
-
-class AuthAuthenticated extends AuthState {
+class Authenticated extends AuthState {
   final User user;
-  AuthAuthenticated(this.user);
+
+  const Authenticated({required this.user});
 
   @override
   List<Object?> get props => [user];
 }
 
-class AuthOtpSent extends AuthState {
-  final String verificationId;
-  final String phoneNumber;
+class Unauthenticated extends AuthState {}
 
-  AuthOtpSent({
-    required this.verificationId,
-    required this.phoneNumber,
-  });
-
-  @override
-  List<Object?> get props => [verificationId, phoneNumber];
-}
-
-class AuthOtpTimerTick extends AuthState {
-  final String verificationId;
-  final String phoneNumber;
-  final int secondsLeft;
-
-  AuthOtpTimerTick({
-    required this.verificationId,
-    required this.phoneNumber,
-    required this.secondsLeft,
-  });
-
-  @override
-  List<Object?> get props => [verificationId, phoneNumber, secondsLeft];
-}
-
-class AuthProfileUpdated extends AuthState {
-  final User user;
-  AuthProfileUpdated(this.user);
-
-  @override
-  List<Object?> get props => [user];
-}
-
-// FIXED: positional constructor — AuthError('message')
 class AuthError extends AuthState {
   final String message;
-  AuthError(this.message);
+
+  const AuthError({required this.message});
 
   @override
   List<Object?> get props => [message];
