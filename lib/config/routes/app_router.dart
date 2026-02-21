e71e49870e3cd1a1b73398e650a8235e4c5334b5
Achini248 <nimeshikaachini22@gmail.com>
2026-02-21 11:19:56 +0530

    fixed app_router.dart

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,53 +1,32 @@
-// lib/config/routes/app_router.dart
-
+//app_router.dart
 import 'package:flutter/material.dart';
-
-import '../../features/home/presentation/pages/splash_screen.dart';
-import '../../features/home/presentation/pages/onboarding_page.dart';
-import '../../features/home/presentation/pages/home_page.dart';
 import '../../features/authentication/presentation/pages/login_page.dart';
 import '../../features/authentication/presentation/pages/signup_page.dart';
-import '../../features/authentication/presentation/pages/otp_verification_page.dart';
-import '../../features/authentication/presentation/pages/profile_settings_page.dart';
+import '../../features/home/presentation/pages/home_page.dart';
+import '../../features/home/presentation/pages/onboarding_page.dart';
+import '../../features/home/presentation/pages/splash_screen.dart';
 import 'route_names.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case RouteNames.splash:
-        return MaterialPageRoute(
-            builder: (_) => const SplashScreen());
-
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      
       case RouteNames.onboarding:
-        return MaterialPageRoute(
-            builder: (_) => const OnboardingPage());
-
+        return MaterialPageRoute(builder: (_) => const OnboardingPage());
+      
       case RouteNames.login:
-        return MaterialPageRoute(
-            builder: (_) => const LoginPage());
-
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+      
       case RouteNames.signup:
-        return MaterialPageRoute(
-            builder: (_) => const SignupPage());
-
-      case RouteNames.otp:
-        final args =
-            settings.arguments as Map<String, dynamic>? ?? {};
-        return MaterialPageRoute(
-          builder: (_) => OtpVerificationPage(
-            phoneNumber: args['phoneNumber'] ?? '',
-            verificationId: args['verificationId'],
-          ),
-        );
-
+        return MaterialPageRoute(builder: (_) => const SignupPage());
+      
       case RouteNames.home:
-        return MaterialPageRoute(
-            builder: (_) => const HomePage());
-
-      case RouteNames.profile:
-        return MaterialPageRoute(
-            builder: (_) => const ProfileSettingsPage());
-
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      
+      // Add more routes as you implement features
+      
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
