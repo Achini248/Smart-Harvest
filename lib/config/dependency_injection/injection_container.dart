50efe563c1e8ffa389099623fdec3108a9f71969
Achini248 <nimeshikaachini22@gmail.com>
2026-03-01 02:05:19 +0530

    fixed

--- lib/config/dependency_injection/injection_container.dart
+++ lib/config/dependency_injection/injection_container.dart
@@ -1,37 +1,38 @@
+// lib/config/dependency_injection/injection_container.dart
 import 'package:get_it/get_it.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-import '../../features/authentication/presentation/bloc/auth_bloc.dart';
+import '../../features/authentication/data/datasources/auth_remote_datasource.dart';
+import '../../features/authentication/data/repositories/auth_repository_impl.dart';
+import '../../features/authentication/domain/repositories/auth_repository.dart';
 import '../../features/authentication/domain/usecases/login_usecase.dart';
 import '../../features/authentication/domain/usecases/register_usecase.dart';
 import '../../features/authentication/domain/usecases/logout_usecase.dart';
-import '../../features/authentication/domain/usecases/get_current_user_usecase.dart';
-import '../../features/authentication/domain/repositories/auth_repository.dart';
-import '../../features/authentication/data/datasources/auth_remote_datasource.dart';
-import '../../features/authentication/data/repositories/auth_repository_impl.dart';
+import '../../features/authentication/presentation/bloc/auth_bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  // data sources
+  // Firebase
+  sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+
+  // Data sources
   sl.registerLazySingleton<AuthRemoteDataSource>(
-      () => AuthRemoteDataSourceImpl());
+    () => AuthRemoteDataSourceImpl(firebaseAuth: sl()),
+  );
 
-  // repositories
+  // Repositories
   sl.registerLazySingleton<AuthRepository>(
-      () => AuthRepositoryImpl(remoteDataSource: sl()));
+    () => AuthRepositoryImpl(remoteDataSource: sl()),
+  );
 
-  // usecases
+  // Use cases
   sl.registerLazySingleton(() => LoginUseCase(sl()));
   sl.registerLazySingleton(() => RegisterUseCase(sl()));
   sl.registerLazySingleton(() => LogoutUseCase(sl()));
-  sl.registerLazySingleton(() => GetCurrentUserUseCase(sl()));
 
-  // blocs
-  sl.registerFactory(() => AuthBloc(
-        loginUseCase: sl(),
-        registerUseCase: sl(),
-        logoutUseCase: sl(),
-        getCurrentUserUseCase: sl(),
-        repository: sl(),
-      ));
+  // Blocs
+  sl.registerFactory(
+    () => AuthBloc(authRepository: sl()),
+  );
 }
