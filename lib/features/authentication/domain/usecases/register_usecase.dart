29f5cb333fe1ca2152fe424bd74a5a5113ff30ad
Achini248 <nimeshikaachini22@gmail.com>
2026-03-01 19:55:41 +0530

    fixed register_usecase.dart

--- lib/features/authentication/domain/usecases/register_usecase.dart
+++ lib/features/authentication/domain/usecases/register_usecase.dart
@@ -8,6 +8,7 @@ class RegisterUseCase {
   final AuthRepository repository;
   const RegisterUseCase(this.repository);
 
+  // මෙතන params.phoneNumber දැන් නිවැරදිව වැඩ කරයි
   Future<Either<Failure, UserEntity>> call(RegisterParams params) =>
       repository.register(params.email, params.password, params.phoneNumber);
 }
@@ -16,13 +17,15 @@ class RegisterParams extends Equatable {
   final String email;
   final String password;
   final String displayName;
+  final String phoneNumber; // මේක අලුතින් ඇතුළත් කළා
 
   const RegisterParams({
     required this.email,
     required this.password,
     required this.displayName,
+    required this.phoneNumber, // Constructor එකටත් ඇතුළත් කළා
   });
 
   @override
-  List<Object?> get props => [email, password, displayName];
+  List<Object?> get props => [email, password, displayName, phoneNumber];
 }
