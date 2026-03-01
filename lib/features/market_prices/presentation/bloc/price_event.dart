e2d215d8e77d449a0443413c393d78be404db975
Achini248 <nimeshikaachini22@gmail.com>
2026-03-01 19:12:15 +0530

    fixed price event

--- lib/features/market_prices/presentation/bloc/price_event.dart
+++ lib/features/market_prices/presentation/bloc/price_event.dart
@@ -2,23 +2,28 @@ import 'package:equatable/equatable.dart';
 
 abstract class PriceEvent extends Equatable {
   const PriceEvent();
+
   @override
   List<Object?> get props => [];
 }
 
-class LoadDailyPricesEvent extends PriceEvent {}
+class LoadDailyPricesEvent extends PriceEvent {
+  // මෙතන 'const' Constructor එක අනිවාර්යයෙන්ම තිබිය යුතුයි
+  const LoadDailyPricesEvent(); 
+}
 
 class LoadPriceTrendsEvent extends PriceEvent {
   final String productName;
   const LoadPriceTrendsEvent(this.productName);
+
   @override
   List<Object?> get props => [productName];
 }
 
-// මේ කොටස අනිවාර්යයෙන්ම තියෙන්න ඕනේ
 class SearchPricesEvent extends PriceEvent {
   final String query;
   const SearchPricesEvent({required this.query});
+
   @override
   List<Object?> get props => [query];
 }
