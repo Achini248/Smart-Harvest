fd474170b262428869830219981b5ab16aa87637
Achini248 <nimeshikaachini22@gmail.com>
2026-03-01 19:59:49 +0530

    Add get_price_trends_usecase.dart

--- lib/features/market_prices/domain/usecases/get_price_trends_usecase.dart
+++ lib/features/market_prices/domain/usecases/get_price_trends_usecase.dart
@@ -1,15 +1,14 @@
 import 'package:dartz/dartz.dart';
 import '../../../../core/errors/failures.dart';
-import '../entities/price.dart';
 import '../repositories/price_repository.dart';
 
 class GetPriceTrendsUseCase {
   final PriceRepository repository;
 
-  GetPriceTrendsUseCase(this.repository);
+  const GetPriceTrendsUseCase(this.repository);
 
-  // බෝගයේ නම (productName) ලබාදී මිල වෙනස්වීමේ දත්ත ලබාගැනීම
-  Future<Either<Failure, List<PriceEntity>>> call(String productName) async {
+  // Return type එක Map එකක් විදිහට වෙනස් කළා
+  Future<Either<Failure, Map<DateTime, double>>> call(String productName) async {
     return await repository.getPriceTrends(productName);
   }
 }
