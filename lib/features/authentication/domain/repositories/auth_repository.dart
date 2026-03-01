31537e32e193c2f72b1a263e4948706f982c0f6b
Achini248 <nimeshikaachini22@gmail.com>
2026-03-01 10:22:17 +0530

    fixed auth_repository.dart

--- lib/features/authentication/domain/repositories/auth_repository.dart
+++ lib/features/authentication/domain/repositories/auth_repository.dart
@@ -1,18 +1,21 @@
 import 'package:dartz/dartz.dart';
-import '../../../../core/error/failures.dart';
-import '../entities/user_entity.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../entities/user.dart';
 
 abstract class AuthRepository {
-  Future<Either<Failure, UserEntity>> login({
-    required String email,
-    required String password,
-  });
+  Future<Either<Failure, UserEntity>> login(
+    String email,
+    String password,
+  );
 
-  Future<Either<Failure, UserEntity>> register({
-    required String name,
-    required String email,
-    required String password,
-  });
+  Future<Either<Failure, UserEntity>> register(
+    String email,
+    String password,
+    String phoneNo,
+  );
 
   Future<Either<Failure, void>> logout();
+
+  Future<UserEntity?> getCurrentUser();
 }
