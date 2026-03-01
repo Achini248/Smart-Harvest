6cb2cc1a68a5c5b3c1ba253b0fe7213077e5a736
Achini248 <nimeshikaachini22@gmail.com>
2026-03-01 10:00:47 +0530

    fixed auth_bloc.dart

--- lib/features/authentication/presentation/bloc/auth_bloc.dart
+++ lib/features/authentication/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/entities/user.dart';
 import '../../domain/repositories/auth_repository.dart';
 import 'auth_event.dart';
 import 'auth_state.dart';
@@ -6,30 +8,38 @@ import 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final AuthRepository authRepository;
 
-  AuthBloc({required this.authRepository}) : super(AuthInitial()) {
+  AuthBloc({required this.authRepository}) : super(const AuthInitial()) {
     on<LoginEvent>(_onLogin);
     on<RegisterEvent>(_onRegister);
     on<LogoutEvent>(_onLogout);
     on<CheckAuthStatusEvent>(_onCheckAuthStatus);
   }
 
-  Future<void> _onLogin(LoginEvent event, Emitter<AuthState> emit) async {
-    emit(AuthLoading());
+  Future<void> _onLogin(
+    LoginEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(const AuthLoading());
     try {
-      final user = await authRepository.login(event.email, event.password);
+      final UserEntity user =
+          await authRepository.login(event.email, event.password);
       emit(Authenticated(user: user));
     } catch (e) {
       emit(AuthError(message: e.toString()));
     }
   }
 
-  Future<void> _onRegister(RegisterEvent event, Emitter<AuthState> emit) async {
-    emit(AuthLoading());
+  Future<void> _onRegister(
+    RegisterEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(const AuthLoading());
     try {
-      final user = await authRepository.register(
-        event.email,
-        event.password,
-        event.phoneNo,
+      final UserEntity user = await authRepository.register(
+        name: event.name,
+        email: event.email,
+        password: event.password,
+        phoneNo: event.phoneNo,
       );
       emit(Authenticated(user: user));
     } catch (e) {
@@ -37,27 +47,32 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     }
   }
 
-  Future<void> _onLogout(LogoutEvent event, Emitter<AuthState> emit) async {
-    emit(AuthLoading());
+  Future<void> _onLogout(
+    LogoutEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(const AuthLoading());
     try {
       await authRepository.logout();
-      emit(Unauthenticated());
+      emit(const Unauthenticated());
     } catch (e) {
       emit(AuthError(message: e.toString()));
     }
   }
 
   Future<void> _onCheckAuthStatus(
-      CheckAuthStatusEvent event, Emitter<AuthState> emit) async {
+    CheckAuthStatusEvent event,
+    Emitter<AuthState> emit,
+  ) async {
     try {
-      final user = await authRepository.getCurrentUser();
+      final UserEntity? user = await authRepository.getCurrentUser();
       if (user != null) {
         emit(Authenticated(user: user));
       } else {
-        emit(Unauthenticated());
+        emit(const Unauthenticated());
       }
-    } catch (e) {
-      emit(Unauthenticated());
+    } catch (_) {
+      emit(const Unauthenticated());
     }
   }
 }
