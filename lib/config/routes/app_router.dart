21d79e49eb1c486bb4e83a0f915dfd9e78de0b1f
Achini248 <nimeshikaachini22@gmail.com>
2026-03-01 18:57:23 +0530

    fixed price_event

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,132 +1,65 @@
 import 'package:flutter/material.dart';
 import 'route_names.dart';
-
-// Authentication feature
 import '../../features/authentication/presentation/pages/login_page.dart';
 import '../../features/authentication/presentation/pages/signup_page.dart';
 import '../../features/authentication/presentation/pages/otp_verification_page.dart';
 import '../../features/authentication/presentation/pages/profile_settings_page.dart';
-
-// Home feature
 import '../../features/home/presentation/pages/splash_screen.dart';
 import '../../features/home/presentation/pages/onboarding_page.dart';
 import '../../features/home/presentation/pages/home_page.dart';
-
-// Crop management feature
 import '../../features/crop_management/presentation/pages/add_crop_page.dart';
 import '../../features/crop_management/presentation/pages/crops_list_page.dart';
 import '../../features/crop_management/presentation/pages/crop_detail_page.dart'; 
-
-// Marketplace feature
 import '../../features/marketplace/presentation/pages/marketplace_home_page.dart';
 import '../../features/marketplace/presentation/pages/my_orders_page.dart';
 import '../../features/marketplace/presentation/pages/order_inbox_page.dart';
-
-// Market prices feature
 import '../../features/market_prices/presentation/pages/daily_market_prices_page.dart';
-
-// Weather feature
 import '../../features/weather/presentation/pages/weather_page.dart';
-
-// Notifications feature
 import '../../features/notifications/presentation/pages/notifications_page.dart';
-
-// Messaging feature
 import '../../features/messaging/presentation/pages/messages_list_page.dart';
 import '../../features/messaging/presentation/pages/chat_page.dart';
-
-// Government dashboard feature
 import '../../features/government_dashboard/presentation/pages/government_dashboard_page.dart';
-
-// Analytics feature
 import '../../features/analytics/presentation/pages/analytics_page.dart';
 
 class AppRouter {
   Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-      // Core
       case RouteNames.splash:
         return _fade(const SplashScreen(), settings);
       case RouteNames.onboarding:
         return _slide(const OnboardingPage(), settings);
       case RouteNames.home:
         return _fade(const HomePage(), settings);
-
-      // Authentication
       case RouteNames.login:
         return _slide(const LoginPage(), settings);
       case RouteNames.signup:
         return _slide(const SignupPage(), settings);
-      case RouteNames.otpVerification:
-        return _slide(const OtpVerificationPage(), settings);
       case RouteNames.profileSettings:
         return _slide(const ProfileSettingsPage(), settings);
-
-      // Crop management
       case RouteNames.myCrops:
-        // 'MyCropsPage' වෙනුවට නිවැරදි Class Name එක 'CropsListPage' ලෙස වෙනස් කළා
         return _slide(const CropsListPage(), settings); 
       case RouteNames.addCrop:
         return _slide(const AddCropPage(), settings);
       case RouteNames.cropDetails:
         final args = settings.arguments as Map<String, dynamic>?;
         return _slide(
-          // 'CropDetailsPage' වෙනුවට නිවැරදි Class Name එක 'CropDetailPage' ලෙස වෙනස් කළා
-          CropDetailPage(
-            cropId: args?['cropId'] as String?,
-          ),
+          CropDetailPage(crop: args?['crop']), 
           settings,
         );
-
-      // Marketplace & prices
       case RouteNames.marketplaceHome:
         return _slide(const MarketplaceHomePage(), settings);
-      case RouteNames.myOrders:
-        return _slide(const MyOrdersPage(), settings);
-      case RouteNames.orderInbox:
-        return _slide(const OrderInboxPage(), settings);
       case RouteNames.dailyMarketPrices:
         return _slide(const DailyMarketPricesPage(), settings);
-
-      // Weather
-      case RouteNames.weatherOverview:
-        return _slide(const WeatherPage(), settings);
-
-      // Notifications
-      case RouteNames.notifications:
-        return _slide(const NotificationsPage(), settings);
-
-      // Messaging
-      case RouteNames.messagesList:
-        return _slide(const MessagesListPage(), settings);
-      case RouteNames.chat:
-        final args = settings.arguments as Map<String, dynamic>?;
-        return _slide(
-          ChatPage(
-            conversation: args?['conversation'], 
-          ),
-          settings,
-        );
-
-      // Government dashboard & analytics
-      case RouteNames.governmentDashboard:
-        return _slide(const GovernmentDashboardPage(), settings);
-      case RouteNames.analytics:
-        return _slide(const AnalyticsPage(), settings);
-
       default:
         return _unknownRoute(settings);
     }
   }
 
-  // Transitions (Fade & Slide)
   Route<dynamic> _fade(Widget page, RouteSettings settings) {
     return PageRouteBuilder(
       settings: settings,
       pageBuilder: (_, __, ___) => page,
-      transitionsBuilder: (_, animation, __, child) {
-        return FadeTransition(opacity: animation, child: child);
-      },
+      transitionsBuilder: (_, a, __, c) => FadeTransition(opacity: a, child: c),
     );
   }
 
@@ -134,23 +67,18 @@ class AppRouter {
     return PageRouteBuilder(
       settings: settings,
       pageBuilder: (_, __, ___) => page,
-      transitionsBuilder: (_, animation, __, child) {
-        final offsetAnimation = Tween<Offset>(
-          begin: const Offset(0.0, 0.05),
-          end: Offset.zero,
-        ).animate(CurvedAnimation(parent: animation, curve: Curves.easeOutCubic));
-        return SlideTransition(position: offsetAnimation, child: child);
+      transitionsBuilder: (_, a, __, c) {
+        return SlideTransition(
+          position: Tween<Offset>(begin: const Offset(0, 0.05), end: Offset.zero).animate(a),
+          child: c,
+        );
       },
     );
   }
 
   Route<dynamic> _unknownRoute(RouteSettings settings) {
     return MaterialPageRoute(
-      settings: settings,
-      builder: (_) => Scaffold(
-        appBar: AppBar(title: const Text('Page not found')),
-        body: Center(child: Text('No route defined for "${settings.name}"')),
-      ),
+      builder: (_) => Scaffold(body: Center(child: Text('Route not found'))),
     );
   }
 }
--- lib/features/market_prices/presentation/bloc/price_event.dart
+++ lib/features/market_prices/presentation/bloc/price_event.dart
@@ -1,22 +1,24 @@
-// lib/features/market_prices/presentation/bloc/price_event.dart
 import 'package:equatable/equatable.dart';
 
 abstract class PriceEvent extends Equatable {
   const PriceEvent();
-
   @override
   List<Object?> get props => [];
 }
 
-class LoadDailyPricesEvent extends PriceEvent {
-  const LoadDailyPricesEvent();
-}
+class LoadDailyPricesEvent extends PriceEvent {}
 
 class LoadPriceTrendsEvent extends PriceEvent {
   final String productName;
-
   const LoadPriceTrendsEvent(this.productName);
-
   @override
   List<Object?> get props => [productName];
+}
+
+// මේ කොටස අනිවාර්යයෙන්ම තියෙන්න ඕනේ
+class SearchPricesEvent extends PriceEvent {
+  final String query;
+  const SearchPricesEvent({required this.query});
+  @override
+  List<Object?> get props => [query];
 }
