b687b27e28b6931dc3cb11a27b560173384d1e5b
Achini248 <nimeshikaachini22@gmail.com>
2026-03-08 10:41:59 +0530

    backend/models/user_model.py

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -11,6 +11,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
+        
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
