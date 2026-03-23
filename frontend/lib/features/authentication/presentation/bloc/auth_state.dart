80e64bea9e0b0dc3e3d893ab306ec202ae7548bc
Riffath19 <imusthafa24@gmail.com>
2026-03-23 15:01:52 +0530

    Update auth_state.dart

--- frontend/lib/features/authentication/presentation/bloc/auth_state.dart
+++ frontend/lib/features/authentication/presentation/bloc/auth_state.dart
@@ -2,7 +2,6 @@ import 'package:equatable/equatable.dart';
 
 abstract class AuthState extends Equatable {
   const AuthState();
-
   @override
   List<Object?> get props => [];
 }
@@ -19,13 +18,11 @@ class Authenticated extends AuthState {
   final String uid;
   final String? email;
   final String? displayName;
-
   const Authenticated({
     required this.uid,
     this.email,
     this.displayName,
   });
-
   @override
   List<Object?> get props => [uid, email, displayName];
 }
@@ -36,9 +33,11 @@ class Unauthenticated extends AuthState {
 
 class AuthError extends AuthState {
   final String message;
-
   const AuthError({required this.message});
-
   @override
   List<Object?> get props => [message];
+}
+
+class PasswordResetSent extends AuthState {
+  const PasswordResetSent();
 }
