1bd7d4142b95bb6a71988eee3f01764f2709218d
Riffath19 <imusthafa24@gmail.com>
2026-03-23 14:20:22 +0530

    Update logger.py

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import logging
-from config import Config
-
-logger = logging.getLogger("smart_harvest")
-logger.setLevel(Config.LOG_LEVEL)
-
-if not logger.handlers:
-    ch = logging.StreamHandler()
-    ch.setLevel(Config.LOG_LEVEL)
-    formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s"
-    )
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-=======
 # backend/utils/logger.py
 import logging, sys
 
@@ -28,4 +12,3 @@ def get_logger(name: str) -> logging.Logger:
         logger.addHandler(handler)
         logger.setLevel(logging.DEBUG)
     return logger
->>>>>>> ddbef5e9db3a8e5ea8f1ef25cdf5bcfa36295850
